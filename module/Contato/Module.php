18c7641f03b0f4de81d13c781b630b9b2fad2530
igor <igor@tice>
2013-01-30 16:29:37 -0300

    segungo commit: base module Contato ok

---
+++ module/Contato/Module.php
@@ -0,0 +1,27 @@
+<?php
+
+# namespace no qual se encontra Module.php
+namespace Contato;
+
+class Module
+{
+
+    # include de arquivo para outras configuracoes
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    # autoloader para namespaces
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+}
